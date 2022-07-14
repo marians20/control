8e4039663c00f96bf61bb9509fadcce064de9597
Marian Spoiala <marian.spoiala@gmail.com>
2022-07-14 06:49:35 +0300

    Small changes at gpio

--- helpers/gpio.js
+++ helpers/gpio.js
@@ -11,6 +11,12 @@ const _pins = {};
 
 const getPins = () => _pins;
 
+/**
+ *
+ * @param {*} pin GPIO pin number
+ * @param {*} direction can be 'in', 'out', 'high' or 'low'
+ * @returns an instance of Gpio
+ */
 const setPin = (pin, direction) => {
     if(direction.toUpperCase() === 'HIGH') {
         _pins[pin] = 1;
@@ -23,18 +29,33 @@ const setPin = (pin, direction) => {
     return new Gpio(pin, direction);
 }
 
+/**
+ * sets GPIO pin output to HIGH
+ * @param {*} pin an instance of Gpio retrieved by using setPin method
+ * @returns an array with known Gpio pins and their latest output values
+ */
 const turnOn = async (pin) => {
     _pins[pin._gpio] = 1;
     await pin.write(Gpio.HIGH);
     return _pins;
 }
 
+/**
+ * sets GPIO pin output to LOW
+ * @param {*} pin an instance of Gpio retrieved by using setPin method
+ * @returns an array with known Gpio pins and their latest output values
+ */
 const turnOff = async (pin) => {
     await pin.write(Gpio.LOW);
     _pins[pin._gpio] = 0;
     return _pins;
 }
 
+/**
+ * toggles the GPIO pin output value
+ * @param {*} pin an instance of Gpio retrieved by using setPin method
+ * @returns an array with known Gpio pins and their latest output values
+ */
 const toggle = async (pin) => {
     const oldValue = _pins[pin._gpio] ?? Gpio.LOW;
     console.log(_pins[pin._gpio], oldValue);
--- pages/api/relays/[relayPin]/[value].js
+++ pages/api/relays/[relayPin]/[value].js
@@ -1,33 +1,33 @@
-import { setPin, turnOff, turnOn, toggle } from '../../../../helpers/gpio';
+import * as gpio from '../../../../helpers/gpio';
 
 export default async function relay(req, res) {
     const pinNumber = Number(req.query.relayPin);
     let response;
     switch (Number(req.query.value)) {
         case 0:
-            response = await turnRelayOff(pinNumber);
+            response = await turnOff(pinNumber);
             break;
         case 1:
-            response = await turnRelayOn(pinNumber);
+            response = await turnOn(pinNumber);
             break;
         default:
-            response = await toggleRelay(pinNumber);
+            response = await toggle(pinNumber);
             break
     }
     res.status(200).json({ ...req.query, response });
 }
 
-async function turnRelayOn(pinNumber) {
-    const pin = setPin(pinNumber, 'out');
-    return await turnOn(pin);
+async function turnOn(pinNumber) {
+    const pin = gpio.setPin(pinNumber, 'out');
+    return await gpio.turnOn(pin);
 }
 
-async function turnRelayOff(pinNumber) {
-    let pin = setPin(pinNumber, 'out');
-    return await turnOff(pin);
+async function turnOff(pinNumber) {
+    let pin = gpio.setPin(pinNumber, 'out');
+    return await gpio.turnOff(pin);
 }
 
-async function toggleRelay(pinNumber) {
-    let pin = setPin(pinNumber, 'out');
-    return await toggle(pin);
+async function toggle(pinNumber) {
+    let pin = gpio.setPin(pinNumber, 'out');
+    return await gpio.toggle(pin);
 }
